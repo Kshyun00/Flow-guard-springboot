87dd9938bf7fd4815ccf4dc99963f3be04a5c657
flsrinn <wjsdkfls03@naver.com>
2026-03-06 10:04:04 +0900

    chore: Maven wrapper, gitignore, 테스트 파일 추가

---
+++ src/test/java/com/skala/process_monitoring_ai/ProcessMonitoringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.process_monitoring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessMonitoringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
